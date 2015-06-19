4f6edd2139bb1c44820aec06f3c9f540ad0f5aff
alex cai <cyy0523xc@gmail.com>
2015-06-19 16:48:32 +0800

    hello world

---
+++ hello_world.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding=utf-#!/usr/bin/env python  
+# -*- coding:utf-8 -*-  
+#  
+#   Authhor :Eric.Tang  
+#   Email   :jeepxiaozi66@gmail.com  
+#   Date    :13/06/02 22:17:57  
+#   Desc    :hello,world of tornado  
+#  
+  
+import tornado.ioloop
+import tornado.web
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("Hello, world")
+
+application = tornado.web.Application([
+    (r"/", MainHandler),
+])
+
+if __name__ == "__main__":
+    application.listen(8889)
+    tornado.ioloop.IOLoop.instance().start()
+
